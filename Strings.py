967a968f89c3ee5557f708577449bdafaf178e82
abdul hasib yousufzai <yousufzai.abdulhasib@gmail.com>
2023-10-11 00:06:15 -0700

    added string class and more code examples in it

--- Strings.py
+++ Strings.py
@@ -4,3 +4,25 @@ other = "Hello world 'from python'"
 print(other)
 new_line ="hi how \n are you doing?"
 print(new_line)
+
+#String concatenation
+str_one ="hi"
+str_two = " how are you"
+str_full =str_one+str_two
+print(str_full)
+
+#string formating
+guess =5
+print(f"your {guess} is wrong!")
+name="chucky"
+print(name[0])
+
+#converting data types
+decimal2 =23.44
+print(type(decimal2))
+integer2 = int(decimal2)
+print(integer2)
+int=23
+float3 =float(int)
+print(float3)
+
