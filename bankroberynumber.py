5bbb31c848cbe35e8faee5d2114aa43b71b6521a
abdul hasib yousufzai <yousufzai.abdulhasib@gmail.com>
2023-10-10 23:23:57 -0700

    initial commit and added first exercise file

---
+++ bankroberynumber.py
@@ -0,0 +1,3 @@
+cash =100000000000334
+# the above cash is stolen by five people equaly divide it
+print(cash/5)
---
+++ main.py
@@ -0,0 +1,6 @@
+result = 2**8
+print(result)
+reminder = 12%5
+print(reminder)
+
+print(10//10)
