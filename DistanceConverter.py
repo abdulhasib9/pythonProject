33f41456444b2480476f35de5e36d286ab67f90a
abdul hasib yousufzai <yousufzai.abdulhasib@gmail.com>
2023-10-11 00:18:09 -0700

    added distanceconverter class and code examples in it

---
+++ DistanceConverter.py
@@ -0,0 +1,5 @@
+print("how many kms ")
+user_input_kms =input()
+miles = float(user_input_kms)/1.60934
+#print("Okay you said "+user_input_kms)
+print(f"Okay you have drove {round(miles,2)} Today!")
