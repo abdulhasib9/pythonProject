f1165d961dbe126b8e50be22322a0b9c8aba87e6
abdul hasib yousufzai <yousufzai.abdulhasib@gmail.com>
2023-10-15 22:21:04 -0700

    added pattern class and some code examples

---
+++ pattern.py
@@ -0,0 +1,9 @@
+for num in range(1,11):
+    print("*"*num)
+
+#printing the same pattern using whileloop
+numbers = 1
+while numbers < 11:
+    print("*"*numbers)
+    print("\U0001F600" *numbers)
+    numbers+=1
