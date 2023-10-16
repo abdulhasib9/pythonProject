3b647bf41e347c7f24ed6ab4d5a69feda7d0f1c0
abdul hasib yousufzai <yousufzai.abdulhasib@gmail.com>
2023-10-15 21:31:37 -0700

    added loops class and some code example

---
+++ RPSMiniproject.py
@@ -0,0 +1,6 @@
+import random
+
+
+rand_num =random.randint(0,2)
+if rand_num ==0:
+    computer="rock"
---
+++ loops.py
@@ -0,0 +1,12 @@
+#for loop
+for item in range(0,9):
+    print(item)
+print("----------------------------------")
+for char in "hello World":
+    print(char)
+
+print("----------------------------------")
+
+r = range(9)
+
+list(r)
