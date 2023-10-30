039d7752a26693a3a697fa6ebb9f8114240f8af4
abdul hasib yousufzai <yousufzai.abdulhasib@gmail.com>
2023-10-29 22:52:59 -0700

    added morenumbers class and some code examples in it

---
+++ morenumbers.py
@@ -0,0 +1,10 @@
+# we use // for ignoring the floating point numbers in opearation 
+print (5/2)
+
+# with // 
+print(5 // 2)
+
+#rounding the numbers to fewer floating points
+number = 2.6666666
+print(round (number,2)) #this will round the floating point numbers to 2
+
--- test.py
+++ test.py
@@ -1,3 +1,6 @@
 num_of_char= len(input("please enter your name: "))
 #print("you name consist of"+num_of_char+" numbers")
 print(f"your name is consist of {num_of_char} Characters")
+
+# // when we dont want the floating points
+print (4/3)
