3ed5b68b6375fbbd50a4b57e7684fbefe467f9ba
abdul hasib yousufzai <yousufzai.abdulhasib@gmail.com>
2023-10-30 23:56:32 -0700

    adde rpcgame class and code examples in it

--- 100dayspython/RPCgame.py
+++ 100dayspython/RPCgame.py
@@ -1,3 +1,4 @@
+import random
 rock = '''
     _______
 ---'   ____)
@@ -25,5 +26,17 @@ scissors = '''
 ---.__(___)
 '''
 
-#Write your code below this line 👇
 
+
+choice = int(input("What do you choos ? Type 0 for Rock, 1 for Paper or 2 for Scissors "))
+computer_choice= random.randint(0,2)
+#print(computer_choice)
+if choice <0 or choice >=2:
+    print("Invalide number! You lose")
+else:
+    if choice ==0 and computer_choice==2:
+        print("You Win!")
+    elif computer_choice >choice:
+        print("Computer Wins!")
+    elif computer_choice == choice:
+        print("Match draw!")
