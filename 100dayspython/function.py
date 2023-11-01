1af4e39e4d6352b3124b3e61b30affba25036a18
abdul hasib yousufzai <yousufzai.abdulhasib@gmail.com>
2023-10-31 22:40:52 -0700

    added function class and some code examples in it

---
+++ 100dayspython/function.py
@@ -0,0 +1,14 @@
+def my_function():
+    print("my firts python function!")
+    
+my_function()
+
+def add(num1,numb2):
+    return numb2+num1
+print(add(23,45))
+
+#while loop 
+limit =8
+while limit >0:
+    print(limit)
+    limit-=1
