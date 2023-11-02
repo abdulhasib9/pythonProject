0ca3a4bf611c00884591a759b7b8763dd44526d8
abdul hasib yousufzai <yousufzai.abdulhasib@gmail.com>
2023-11-01 23:44:09 -0700

    added dictionariesdemo class and code example

---
+++ 100dayspython/dictionariesdemo.py
@@ -0,0 +1,23 @@
+dic ={
+    "name":"abdulhasib",
+    "last_name":"yousufzai"
+    }
+
+print(dic["name"])
+
+#adding entry
+dic["full_name"]="abdul Hasib Yousufzai"
+print(dic)
+
+#clearing all data from dictionary 
+    # dic={}
+    # print(dic)
+    
+#looping
+for key in dic:
+    print(key + " : " + dic[key] +"\n")
+   # print(dic[key])
+
+
+
+
