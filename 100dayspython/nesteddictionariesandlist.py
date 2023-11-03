fe509497761ae7f3ef38c1fa896dbbe912f263a3
abdul hasib yousufzai <yousufzai.abdulhasib@gmail.com>
2023-11-02 22:12:27 -0700

    added nesteddictionariesandlist class and code example

---
+++ 100dayspython/nesteddictionariesandlist.py
@@ -0,0 +1,10 @@
+travel_logs={
+    "France":["Paris","lille","Dijon"],
+    "Afghanistan":["Kabul","Nangarhar"]
+}
+
+nested_dic ={
+    "nested":{"City":"kabul"}
+}
+
+print(travel_logs["France"][0])
