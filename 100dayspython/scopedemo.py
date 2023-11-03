122d1baaade31feff8ed2b5c86918c681477a79e
abdul hasib yousufzai <yousufzai.abdulhasib@gmail.com>
2023-11-02 23:24:23 -0700

    added scopedemo class and code example

--- 100dayspython/scopedemo.py
+++ 100dayspython/scopedemo.py
@@ -1,8 +1,16 @@
 enemies = 1
 
 def increase_enemies():
+  global enemies
   enemies = 2
   print(f"enemies inside function: {enemies}")
 
 increase_enemies()
 print(f"enemies outside function: {enemies}")
+
+# def mainfunction():
+#    def main2():
+#     print("inner main function")
+    
+# mainfunction.main2()
+
