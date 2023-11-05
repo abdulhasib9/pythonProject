adf334818e2cce9a3d1492ffc9e0d046339d258e
abdul hasib yousufzai <yousufzai.abdulhasib@gmail.com>
2023-11-04 22:52:37 -0700

    added code demo into main class

---
+++ 100dayspython/oop/main.py
@@ -0,0 +1,12 @@
+# import turtle
+
+# timmy =turtle.Turtle()
+# import tkinter 
+from turtle import Turtle,Screen
+timmy = Turtle()
+timmy.shape("turtle")
+timmy.color("red")
+timmy.forward(100)
+# print(timmy)
+my_screen = Screen()
+my_screen.exitonclick()
