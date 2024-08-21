f569680566ab8a373b3ea3b23d2e71af321e7b76
abdul hasib yousufzai <yousufzai.abdulhasib@gmail.com>
2024-08-21 07:30:22 -0700

    added more examples in lambda class

---
+++ newcourse/files/map.py
@@ -0,0 +1,19 @@
+numbers = [1,3,4,32,44]
+
+def square(x):
+    return x*x
+
+print(list(map(square,numbers)))
+
+#more map examples
+string_numbers =['1','2','23','23']
+print(string_numbers)
+converted_to_numbers = list(map(int,string_numbers))
+print(converted_to_numbers)
+
+#applying mathematical operation using lambda functions
+
+list1 =[2,3,4,5]
+
+sq = list(map(lambda x: x**x ,list1))
+print(sq)
